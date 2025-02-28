f3bc9f71a7e94614bfec5824ef451e839dd2320a
unknown <buldozerom@gmail.com>
2025-02-28 04:03:12 +0100

    Initial commit

---
+++ Kingrice/Controllers/ContactForm.cs
@@ -0,0 +1,43 @@
+﻿using Kingrice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+//https://navody.webotvurci.cz/kb/nastaveni-smtp-pro-odesilani-emailu/
+//https://www.bing.com/videos/riverview/relatedvideo?q=jak+nastavit+smtp+pro+p%c5%99epos%c3%adl%c3%a1n%c3%ad+emailu+.net+visual+studio+umbraco&mid=162A54D970065E943877162A54D970065E943877&mcid=F31BD7763ED54A6B8D02D214829FC1A1&FORM=VIRE
+
+
+namespace Kingrice.Controllers
+{
+    public class ContactFormController : SurfaceController
+    {
+        [HttpPost]
+        public ActionResult SubmitForm(ContentForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mailMessage = new MailMessage(model.Email, "buldozerom@gmail.com");
+                mailMessage.Subject = model.Subject;
+                mailMessage.Body = model.Message;
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 465);
+                smtpClient.Send(mailMessage);
+                TempData["ContactResult"] = "Has been sent";
+                return RedirectToCurrentUmbracoPage();
+            }
+            //MailMessage mailMessage = new MailMessage(model.Email, "buldozerom@gmail.com");
+            //mailMessage.Subject = model.Subject;
+            //mailMessage.Body = model.Message;
+
+            //SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 465);
+            //smtpClient.Send(mailMessage);
+
+            //return RedirectToCurrentUmbracoPage();
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ Kingrice/Models/ContentForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kingrice.Models
+{
+    public class ContentForm
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
